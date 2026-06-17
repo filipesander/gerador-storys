18f33acd9fcf4e4d85105f93a8ca5748e1db0313
filipesander <desenvolvedorfilipe@gmail.com>
2026-06-16 21:30:40 -0300

    fix: mudado id da planilha✅

--- config/agenda.php
+++ config/agenda.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'sheet_id' => env('AGENDA_SHEET_ID', '14BBRNznsqfw1z2CLw-dC1t6F8FuP0x4SREUMXbb6l1Y'),
+    'sheet_id' => env('AGENDA_SHEET_ID', '1KZnWxGHmy3Mtn-1ZPDU0Lm1Q91MyVyI9xlqc-Z9TL48'),
     'sheet_gid' => env('AGENDA_SHEET_GID', '614191594'),
     'cache_seconds' => (int) env('AGENDA_CACHE_SECONDS', 60),
     'brand' => env('AGENDA_BRAND', 'Thay'),
